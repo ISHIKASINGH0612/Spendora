095909a1915550ead9dbd575b11ca7ff892a5d84
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2025-12-24 17:26:58 +0530

    added env file and installed clerk, created authentication page through clerk

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });//here i am using my own font from google fonts
 
@@ -14,11 +15,12 @@ export default function RootLayout({ children }) {
       <body
         className={`Inter ${inter.className}`}>
         {/* Header */}
-        {children}
+        <Header />
+        <main className="min-h-screen"> {children} </main>
         {/* Footer */}
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>© 2026 Spendora. All rights reserved.</p>
+            <p>© 2026 Spendora. All rights reserved 😀.</p>
           </div>
         </footer>
       </body>
